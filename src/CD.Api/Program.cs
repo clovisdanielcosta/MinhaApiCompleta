6482d7001066938d566fdbdef82515ddc60760e1
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-11 16:43:24 -0300

    Added DbContext and DefaultConnection cofigurations on Program

--- src/CD.Api/Program.cs
+++ src/CD.Api/Program.cs
@@ -1,8 +1,19 @@
 using CD.Api.Configuration;
+using CD.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddDbContext<MeuDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
