12dfa63b566adffec7f1a948e5e095178c2e11fd
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-11 14:04:53 -0300

    Added Auto Mapper to mapping classes Fornecedor, Endereco and Produto with its ViewModels

---
+++ src/CD.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CD.Business.Models;
+using MinhaAPICompleta.ViewModels;
+
+namespace CD.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
