67ab21fdcb0e69b1573ec40d43a7dfc0e71de223
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-11 11:45:38 -0300

    Created MainController and ViewModels

--- src/CD.Api/Controllers/MainController.cs
+++ src/CD.Api/Controllers/MainController.cs
@@ -1,4 +1,6 @@
+using CD.Business.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using MinhaAPICompleta.ViewModels;
 
 namespace CD.Api.Controllers
 {
@@ -11,7 +13,17 @@ namespace CD.Api.Controllers
     [Route("api/[controller]")]
     public class FornecedoresController : MainController
     {
+        private readonly IFornecedorRepository _fornecedorRepository;
 
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+
+        }
     }
 
 }
