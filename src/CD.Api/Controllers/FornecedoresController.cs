491cc35e266210974ae7f8e5de835f2730270c2c
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-12 13:05:30 -0300

    Created methods ObterPorId and ObterFornecedorProdutosEndereco

--- src/CD.Api/Controllers/FornecedoresController.cs
+++ src/CD.Api/Controllers/FornecedoresController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CD.Business.Interfaces;
+using CD.Data.Repository;
 using Microsoft.AspNetCore.Mvc;
 using MinhaAPICompleta.ViewModels;
 using System.Collections.Generic;
@@ -24,6 +25,19 @@ namespace CD.Api.Controllers
             var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             return fornecedor;
         }
+
+        public async Task<FornecedorViewModel> ObterPorid(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            return fornecedor;
+        }
+
+        public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+
     }
 
 }
