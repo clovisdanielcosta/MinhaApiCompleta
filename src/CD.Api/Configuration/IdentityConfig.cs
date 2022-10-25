d5edffd55ea048b84d82875f294b2442d98b501d
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-25 17:06:18 -0300

    Created class identityconfig and updated Program to implement Configuration

---
+++ src/CD.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace CD.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
--- src/CD.Api/Program.cs
+++ src/CD.Api/Program.cs
@@ -4,10 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
 //Configure Services
 
 builder.Services.AddDbContext<MeuDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{ 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentityConfiguration(builder.Configuration);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
