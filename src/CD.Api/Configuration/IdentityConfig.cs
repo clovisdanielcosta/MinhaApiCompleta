4899923b352b89e0ae3b2d4db83f027fedecf61f
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-25 17:37:10 -0300

    Created classes IdentityConfig and ApplicationDbContext

--- src/CD.Api/Configuration/IdentityConfig.cs
+++ src/CD.Api/Configuration/IdentityConfig.cs
@@ -1,9 +1,14 @@
-﻿namespace CD.Api.Configuration
+﻿using CD.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CD.Api.Configuration
 {
     public static class IdentityConfig
     {
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             return services;
         }
