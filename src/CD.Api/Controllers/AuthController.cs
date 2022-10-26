e0b7415a50cc7fe92cc5b85f240c84411ed27988
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-26 10:37:05 -0300

    Created method Registrar on AuthController

--- src/CD.Api/Controllers/AuthController.cs
+++ src/CD.Api/Controllers/AuthController.cs
@@ -1,11 +1,47 @@
-﻿using CD.Business.Interfaces;
+﻿using CD.Api.ViewModels;
+using CD.Business.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CD.Api.Controllers
 {
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
+        private readonly SignInManager<IdentityUser> _signInManger;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(INotificador notificador, 
+                              SignInManager<IdentityUser> signInManger, 
+                              UserManager<IdentityUser> userManager) : base(notificador)
         {
+            _signInManger = signInManger;
+            _userManager = userManager;
+        }
+
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if(!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManger.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
+            
+            return CustomResponse(registerUser);
         }
     }
 }
