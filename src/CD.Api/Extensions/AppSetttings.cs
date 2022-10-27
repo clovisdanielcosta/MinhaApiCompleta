1cd1fc9c097a5f1d71221221a3af6cd6cfe2d7ec
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-27 07:59:50 -0300

    Created class AppSettings on folder Entensions

---
+++ src/CD.Api/Extensions/AppSetttings.cs
@@ -0,0 +1,10 @@
+﻿namespace CD.Api.Extensions
+{
+    public class AppSetttings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
