f6384738fc30972a7ebbde0ddd63b429a44f8a1a
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-11-01 16:33:27 -0300

    Installed Elmah.io to apply log monitoring

--- src/CD.Api/V2/TesteController.cs
+++ src/CD.Api/V2/TesteController.cs
@@ -8,13 +8,23 @@ namespace CD.Api.V2.Controllers
     [Route("api/v{version:apiVersion}/teste")]
     public class TesteController : MainController
     {
-        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser)
+        private readonly ILogger _logger;
+
+        public TesteController(INotificador notificador, IUser appUser, ILogger<TesteController> logger) : base(notificador, appUser)
         {
+            _logger = logger;
         }
 
         [HttpGet]
         public string Valor()
         {
+            _logger.LogTrace("Log Trace");
+            _logger.LogDebug("Log de Debug");
+            _logger.LogInformation("Log de Informação");
+            _logger.LogWarning("Log de Aviso");
+            _logger.LogError("Log de Erro");
+            _logger.LogCritical("Logo de Aviso Crítico");
+
             return "Sou a versão V2";
         }
     }
